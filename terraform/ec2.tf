0dfc08fe943662c727622aa6fa671a36aa4fd10b
Anil Bamnote <anilbamnote21@gmail.com>
2025-10-28 16:28:16 +0530

    tf_code_added | AB

---
+++ terraform/ec2.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+   
+}
+
+
+
+
+resource "aws_instance" "demo" {
+    ami = "ami-02d26659fd82cf299"
+    instance_type = "t3.micro"
+    key_name = "mumbai-soheb"
+    security_groups = ["default"]
+
+
+    tags = {
+       Name = "my-terr-instance" 
+    }
+}
