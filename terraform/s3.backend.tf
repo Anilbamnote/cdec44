3c4d615dfd27dca26882073cb741f72b9a2b2959
Anil Bamnote <anilbamnote21@gmail.com>
2025-11-06 16:10:39 +0530

    s3-backend-stage-added | AB

--- terraform/s3.backend.tf
+++ terraform/s3.backend.tf
@@ -1,27 +1,45 @@
 provider "aws" {
-  region = "ap-south-1"  # Specify the AWS region
+  region = "ap-south-1" # Specify the AWS region
 }
 
 
 
-terraform {
-  backend "s3" {
-    bucket         	   = "new-demo-s3-backend09"
-    key                    = "state/terraform.tfstate"
-    region         	   = "ap-south-1"
-    encrypt        	   = true
-    dynamodb_table = "new"
-  }
-}
+
 
 
 resource "aws_instance" "my_instance" {
+  for_each = var.instance_type
   ami             = "ami-02b8269d5e85954ef"
-  instance_type          = "t3.micro"
+  instance_type   = each.value
   key_name        = "mumbai-soheb"
-  security_groups = ["default"]  # Ensure this security group exists
+  security_groups = ["default"] # Ensure this security group exists
+  # count = 3
 
   tags = {
-    Name = "My-Terraform-Instance"
+    ENV = terraform.workspace
+    Name = "My-Terraform-Instance-${each.key}"
   }
 }
+variable "instance_type"{
+  default = {
+    small = "t3.small"
+    micro = "t3.micro"
+  }
+}
+
+
+
+
+
+
+
+
+# terraform {
+#   backend "s3" {
+#     bucket         = "new-demo-s3-backend09"
+#     key            = "state/terraform.tfstate"
+#     region         = "ap-south-1"
+#     encrypt        = true
+#     dynamodb_table = "new"
+#   }
+# }
